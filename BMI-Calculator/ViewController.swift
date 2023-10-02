30520bc28b6563ff84fa5c46330b8c5f07120615
marufkhan09 <maruf1609@gmail.com>
2023-10-02 16:13:05 +0600

    Initial Commit

---
+++ BMI-Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI-Calculator
+//
+//  Created by Maruf Khan on 2/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
